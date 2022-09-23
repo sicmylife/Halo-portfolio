6e919a46b2a5280a7655456d81726398806e663b
Daniel <sicmylife@gmail.com>
2022-09-23 18:38:30 +0200

    2\3 commit

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
+const nav = document.querySelector('.nav-bar');
+const burger = document.querySelector('.burger');
+const close1 = document.querySelector('.close');
+
+console.log(nav)
+
+burger.addEventListener('click', function(e) {
+e.preventDefault();
+    nav.classList.add('active')
+    close1.classList.add('active')
+})
+
+close1.addEventListener('click', function(e) {
+    e.preventDefault();
+        nav.classList.remove('active')
+        close1.classList.remove('active')
 
 
+    })
+    
 
