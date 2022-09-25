42738d4d7da692973de631eb0cbbb00dc24232ff
Daniel <sicmylife@gmail.com>
2022-09-25 18:35:06 +0200

    454 commit

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const nav = document.querySelector('.nav-bar');
 const burger = document.querySelector('.burger');
 const close1 = document.querySelector('.close');
+const contact = document.querySelector('#contact');
+const overlay = document.querySelector('.overlay');
 
-console.log(nav)
 
 burger.addEventListener('click', function(e) {
 e.preventDefault();
@@ -14,7 +15,12 @@ close1.addEventListener('click', function(e) {
     e.preventDefault();
         nav.classList.remove('active')
         close1.classList.remove('active')
+    })
 
+    contact.addEventListener('click', function(e) {
+overlay.classList.add('contact')
+overlay.innerHTML = "<h2>Hola</h2>"
+        console.log(container)
 
     })
     
