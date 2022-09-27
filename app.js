b54aa0614d171949dd4bb7963aacea414de683d6
Daniel <sicmylife@gmail.com>
2022-09-27 13:08:47 +0200

    45 commit

--- app.js
+++ app.js
@@ -1,27 +1,32 @@
-const nav = document.querySelector('.nav-bar');
-const burger = document.querySelector('.burger');
-const close1 = document.querySelector('.close');
-const contact = document.querySelector('#contact');
-const overlay = document.querySelector('.overlay');
+const nav = document.querySelector(".nav-bar");
+const burger = document.querySelector(".burger");
+const close1 = document.querySelector(".close");
+const contact = document.querySelector("#contact");
+const overlay = document.querySelector(".overlay");
 
 
-burger.addEventListener('click', function(e) {
-e.preventDefault();
-    nav.classList.add('active')
-    close1.classList.add('active')
-})
+burger.addEventListener("click", function (e) {
+  e.preventDefault();
+  nav.classList.add("active");
+  close1.classList.add("active");
+});
 
-close1.addEventListener('click', function(e) {
-    e.preventDefault();
-        nav.classList.remove('active')
-        close1.classList.remove('active')
-    })
+close1.addEventListener("click", function (e) {
+  e.preventDefault();
+  nav.classList.remove("active");
+  close1.classList.remove("active");
+  // closing overlay contact
+  overlay.style.display = "none";
+  close1.classList.remove("active");
 
-    contact.addEventListener('click', function(e) {
-overlay.classList.add('contact')
-overlay.innerHTML = "<h2>Hola</h2>"
-        console.log(container)
+});
 
-    })
-    
+contact.addEventListener("click", function (e) {
+    overlay.style.display = "block";
+  close1.classList.add("active");
 
+  });
+
+
+
+ 
