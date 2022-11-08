201e2032490566f37612775f41b0e334af16c2bd
Daniel <sicmylife@gmail.com>
2022-11-08 14:20:28 +0100

    3 commit

--- portfolio.js
+++ portfolio.js
@@ -16,8 +16,8 @@
     
   })
 
-  tl.from(".char",{ y:130,stagger:0.1 ,ease:"black.out(1.5)"})
-  tl.to(".intro",{ y:-2000,ease:"black.out(1.5)"})
+  tl.from(".char",{ y:130,stagger:1,ease:"black.out(1.5)"})
+  tl.to(".intro",{ y:-2000,duration:2,ease:"black.out(1.5)"})
   tl.fromTo(".grid",{opacity:0, y:200},{opacity:1, y:0,x:0 ,duration:2})
 
 
